a05114752a8218098f4579866c315abd71277aa2
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-01-30 04:19:37 +0000

    added config, typeorm initializations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
+import { TutorModule } from './tutor/tutor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      port: 3306,
+      database: 'ekolance-lms',
+      entities: [],
+      synchronize: true,
+    }),
+    AdminModule,
+    StudentModule,
+    TutorModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
