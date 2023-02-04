9eb1a6f6e36e5593f91c9ae7efc9bc27f4d6251a
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-02-04 01:09:13 +0000

    swagger

--- src/admin/controllers/announcements/announcements.controller.ts
+++ src/admin/controllers/announcements/announcements.controller.ts
@@ -1,21 +1,25 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 import { CreateAnnouncementDTO } from 'src/admin/dtos/CreateAnnouncementDTO';
 import { UpdateAnnouncementDTO } from 'src/admin/dtos/UpdateAnnouncementDTO';
 import { AnnouncementsService } from 'src/admin/services/announcements/announcements.service';
+import { AnnouncementEntity } from 'src/typeorm/entities/AnnouncementEntity';
 
 @Controller('announcements')
 export class AnnouncementsController {
   constructor(private announcementService: AnnouncementsService) {}
 
   @Get()
+  @ApiOkResponse({ status: 200, type: AnnouncementEntity, isArray: true })
   getAllAnnouncements() {
     return this.announcementService.getAllAnnouncements();
   }
@@ -34,8 +38,13 @@ export class AnnouncementsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateAnnouncementDTO: UpdateAnnouncementDTO,
   ) {
-    this.announcementService.updateAnnouncement(id, {
+    return this.announcementService.updateAnnouncement(id, {
       ...updateAnnouncementDTO,
     });
+  }
+
+  @Delete('delete/:id')
+  deleteAnnouncement(@Param('id', ParseIntPipe) id: number) {
+    this.announcementService.deleteAnnouncement(id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Ekolance LMS API')
+    .setDescription('Complete API Backend for the Ekolance LMS Project')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(configService.get<number>('PORT') || 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
