0b0829436b521bc8ffc54640198f81f0a80d78d2
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-02-05 18:31:39 +0000

    tutor announcement done

--- src/tutor/controllers/announcements/announcements.controller.ts
+++ src/tutor/controllers/announcements/announcements.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { CreateAnnouncementDTO } from 'src/admin/dtos/CreateAnnouncementDTO';
+import { UpdateAnnouncementDTO } from 'src/admin/dtos/UpdateAnnouncementDTO';
+import { AnnouncementsService } from 'src/admin/services/announcements/announcements.service';
+import { AnnouncementEntity } from 'src/typeorm/entities/AnnouncementEntity';
 
 @Controller('announcements')
-export class AnnouncementsController {}
+export class AnnouncementsController {
+  constructor(private announcementService: AnnouncementsService) {}
+
+  @Get()
+  @ApiOkResponse({ status: 200, type: AnnouncementEntity, isArray: true })
+  getAllAnnouncements() {
+    return this.announcementService.getAllAnnouncements();
+  }
+
+  @Post('create')
+  async createAnnouncement(
+    @Body() createAnnouncementDTO: CreateAnnouncementDTO,
+  ) {
+    return await this.announcementService.createAnnouncement({
+      ...createAnnouncementDTO,
+    });
+  }
+
+  @Put('update/:id')
+  updateAnnouncement(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAnnouncementDTO: UpdateAnnouncementDTO,
+  ) {
+    return this.announcementService.updateAnnouncement(id, {
+      ...updateAnnouncementDTO,
+    });
+  }
+
+  @Delete('delete/:id')
+  deleteAnnouncement(@Param('id', ParseIntPipe) id: number) {
+    this.announcementService.deleteAnnouncement(id);
+  }
+}
--- src/tutor/dtos/CreateAnnouncementDTO.ts
+++ src/tutor/dtos/CreateAnnouncementDTO.ts
@@ -1,1 +1,21 @@
-export class CreateAnnouncementDTO {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateAnnouncementDTO {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  programmeId: number;
+
+  @IsOptional()
+  tutorId: number;
+
+  @IsOptional()
+  adminId: number;
+}
