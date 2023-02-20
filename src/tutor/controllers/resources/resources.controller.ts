c5413f8d7c281c6b43a4803d0ee770ae37c3f306
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-02-20 02:17:17 +0000

    started working on tutors

--- src/tutor/controllers/resources/resources.controller.ts
+++ src/tutor/controllers/resources/resources.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ResourcesService } from 'src/tutor/services/resources/resources.service';
 
 @Controller('resources')
-export class ResourcesController {}
+export class ResourcesController {
+  constructor(private resourceService: ResourcesService) {}
+}
