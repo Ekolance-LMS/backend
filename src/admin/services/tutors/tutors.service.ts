39e5c2d59f2dc9d2f44ba8c14f637e9662de14de
Etette Etok <etokoffice@gmail.com>
2023-03-01 18:04:54 +0100

    updated tutors.ts

--- src/admin/services/tutors/tutors.service.ts
+++ src/admin/services/tutors/tutors.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TutorEntity } from 'src/typeorm/entities/TutorEntity';
+import { CreateTutorParams, UpdateTutorParams } from 'src/utils/types';
+import { Repository } from 'typeorm';
+import * as ethUtil from 'ethereumjs-util';
+import * as sigUtil from '@metamask/eth-sig-util';
 
 @Injectable()
-export class TutorsService {}
+export class TutorsService {
+  constructor(
+    @InjectRepository(TutorEntity)
+    private tutorRepository: Repository<TutorEntity>,
+  ) {}
+
+  addTutor(createTutorDetails: CreateTutorParams) {
+    //const generatedNonce: number = Math.floor(Math.random() * 10000000000);
+    const tutor = this.tutorRepository.create({
+      ...createTutorDetails,
+     // nonce: generatedNonce,
+    });
+    return this.tutorRepository.save(tutor);
+  }
+
+  updateTutor(id: number, updateTutorDetails: UpdateTutorParams) {
+    return this.tutorRepository.update(
+      { id: id },
+      { ...updateTutorDetails },
+    );
+  }
+
+  loginTutor(publicAddress: string) {
+    //check if user is registered then login else register
+    // validate if address is in db->elsegoto signup page
+    const tutor = this.tutorRepository.findOne({
+      where: { walletAddress: publicAddress },
+    });
+
+    return tutor;
+  }
+
+  delete_tutor(id: number) {
+    return this.tutorRepository.delete({ id });
+  }
+
+//   removeTutor(publicAddress: string) {
+//     // find tutor by wallet address
+//     // remove wallet as tutor
+//     let tutor = this.tutorRepository.delete(
+//        publicAddress);
+//     return tutor;
+//   }
+
+  getAllTutors() {
+    return this.tutorRepository.find();
+  }
+}
